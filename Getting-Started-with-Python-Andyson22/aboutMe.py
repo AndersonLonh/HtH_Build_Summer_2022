6c78297eb082b6788e231c1ec744d02440011ecf
AndersonLonh <anderson.lonh07@gmail.com>
2022-08-31 21:06:40 -0700

    Machine Learning Algorithm

--- Getting-Started-with-Python-Andyson22/aboutMe.py
+++ Getting-Started-with-Python-Andyson22/aboutMe.py
@@ -6,7 +6,7 @@ haveUsedPythonBefore = True
 favoriteHobbies = ["basketball", "Volleyball", "Dance"]
 
 favoriteThings = {
-  "Sports" : favoriteHobbies[1], 
+  "Sports" : [favoriteHobbies[1], "ball"], 
   "Tv Show": "Grey's Anatomy", 
   "Video Game" : "Call of Duty"
 }
@@ -16,6 +16,7 @@ print(fullName, favoriteNickname, age,haveUsedPythonBefore, "\n")
 
 print(favoriteHobbies, "\n")
 
-print(favoriteThings)
+print(favoriteThings['Sports'][0])
 
 all_vars = dict(vars())
+
